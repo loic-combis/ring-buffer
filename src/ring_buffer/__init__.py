6c3f88fb2ecef5634ae83c01b6ecbb3b6379b38f
Loïc Combis <combis.loic@gmail.com>
2026-03-04 19:22:04 +0100

    v1.0.1

--- src/ring_buffer/__init__.py
+++ src/ring_buffer/__init__.py
@@ -3,6 +3,6 @@ from .abstract_ring_buffer import RingBuffer
 from .bytes.bytes_ring_buffer import BytesRingBuffer
 from .bytes.bytes_shm_ring_buffer import BytesShmRingBuffer
 
-__version__ = "0.1.0"
+__version__ = "1.0.1"
 
 __all__ = ["RingBuffer", "BytesRingBuffer", "BytesShmRingBuffer", "__version__"]
